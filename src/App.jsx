c622dc6ae4316d0c6ecf4dcd4b5fbb9b640415c5
Sarman-Keshwani <sarmkesh@gmail.com>
2025-07-06 14:28:41 +0530

    Added Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">Hello World</div>
+    </>
+  );
+}
+
+export default App;
