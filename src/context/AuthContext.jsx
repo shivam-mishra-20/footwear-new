a1d27182c615bd46d498fbceef56005e56a13683
shivam mishra <shivammishra20305@gmail.com>
2025-09-22 17:19:38 +0530

    first commit

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
